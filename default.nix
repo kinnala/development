300760ee24dc24bebbf1d3f3630a8e7c3832404f
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-05-08 11:28:35 +0300

    Add nixops config for virtualbox

---
+++ default.nix
@@ -0,0 +1,9 @@
+{
+  machine =
+    { config, pkgs, ... }:
+    {
+      deployment.targetEnv = "virtualbox";
+      deployment.virtualbox.memorySize = 1024;
+      deployment.virtualbox.vcpu = 2;
+    };
+}
